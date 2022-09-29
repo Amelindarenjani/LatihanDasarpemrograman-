d3aa16e013de6e917e1048fb2871c8c7481a5e38
Amelindarenjani <114630196+Amelindarenjani@users.noreply.github.com>
2022-09-29 11:07:06 +0700

    Add files via upload

---
+++ aplikasibiodata.py
@@ -0,0 +1,15 @@
+# aplikasi biodata  create by Amelinda
+
+# masukan nama
+print("lengkapi data diri anda!")
+
+#input data diri
+nama = input("nama : ")
+kelas = input("kelas : ")
+prodi = input("program studi : ")
+
+print("apakah data dibawah ini sudah benar ?")
+
+#print / output data diri
+print("nama anda adalah: ", nama)
+print("kelas anda adalah: ", kelas)
