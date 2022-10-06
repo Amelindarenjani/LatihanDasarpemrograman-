4306d7bc612d6f14b3b72e7b1dee3d72eba4ff03
Amelindarenjani <114630196+Amelindarenjani@users.noreply.github.com>
2022-10-06 09:09:21 +0700

    Add files via upload

---
+++ tugas_sesi3.py
@@ -0,0 +1,15 @@
+# Biodata sederhana by Amelinda Renjani_TI 22A
+
+nama = "Amelinda Renjani"
+kelas = " TI22A "
+jurusan = "Teknik Informatika"
+quote = "goal without plan is just a dream"
+
+print("nama : ", nama)
+print("kelas : ", kelas)
+print("jurusan :", jurusan)
+print("quote :", quote)
+
+
+
+
